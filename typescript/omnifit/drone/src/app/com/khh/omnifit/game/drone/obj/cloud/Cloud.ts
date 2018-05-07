ad5ce9946ee73eb94352b2d76dcdd6a452a9cada
khh <khh@omnicns.com>
2018-05-08 00:01:49 +0900

    cloud, ground

--- typescript/omnifit/drone/src/app/com/khh/omnifit/game/drone/obj/cloud/Cloud.ts
+++ typescript/omnifit/drone/src/app/com/khh/omnifit/game/drone/obj/cloud/Cloud.ts
@@ -10,6 +10,11 @@ import {DroneStage} from '../../stage/DroneStage';
 // import { Point } from '../org/Point';
 export class Cloud extends ObjDrone{
 
+  private maxX: number = 50;
+  private currentX: number = 0;
+  private beforeWind: number = 0;
+  private wind: number = 0;
+
   constructor(stage: DroneStage, x: number, y: number, z: number, canvas: HTMLCanvasElement) {
     super(stage, x, y, z, canvas);
     this.img = new Image();
@@ -20,8 +25,16 @@ export class Cloud extends ObjDrone{
   onDraw(): void {
     const x = this.canvas.width / 2;
     const y = this.canvas.height / 2;
+
+    if(this.beforeWind - this.wind>0){
+      this.currentX += 0.1;
+    }else if(this.beforeWind - this.wind<0){
+      this.currentX -= 0.1;
+    }
+
+
     let ctxBuffer: CanvasRenderingContext2D = this.canvas.getContext("2d");
-    ctxBuffer.drawImage(this.img,x - this.img.width/2, 0);
+    ctxBuffer.drawImage(this.img,(x - this.img.width/2) + this.currentX, 0);
   }
 
   clockSignal(value?: any) {
@@ -29,6 +42,14 @@ export class Cloud extends ObjDrone{
   }
 
   intentSignal(intent: Intent<GameData>) {
+    this.onDraw();
   }
 
+  intentSignal(intent: Intent<GameData>) {
+    if(this.beforeWind!=intent.data.wind.x){
+      this.beforeWind=intent.data.wind.x
+    }else{
+      this.wind=intent.data.wind.x
+    }
+  }
 }
--- typescript/omnifit/drone/src/app/com/khh/omnifit/game/drone/obj/ground/Ground.ts
+++ typescript/omnifit/drone/src/app/com/khh/omnifit/game/drone/obj/ground/Ground.ts
@@ -1,16 +1,21 @@
 import { Observable } from 'rxjs/Observable';
-import { Point } from 'app/com/khh/graphics/Point';
-import { Obj } from 'app/com/khh/obj/Obj';
-import {ObjImg} from 'app/com/khh/graphics/ObjImg';
-import {Rect} from 'app/com/khh/graphics/Rect';
+import { Point } from '../../../../../graphics/Point';
+import { Obj } from '../../../../../obj/Obj';
+import {ObjImg} from '../../../../../graphics/ObjImg';
+import {Rect} from '../../../../../graphics/Rect';
 import {ObjDrone} from '../ObjDrone';
 import {Intent} from '../../../../../data/Intent';
 import {GameData} from '../../vo/GameData';
 import {DroneStage} from '../../stage/DroneStage';
+import {PointVector} from '../../../../../math/PointVector';
 // import { Point } from '../org/Point';
 export class Ground extends ObjDrone{
 
 
+  private maxX: number = 50;
+  private currentX: number = 0;
+  private beforeWind: number = 0;
+  private wind: number = 0;
   constructor(stage: DroneStage,x: number, y: number, z: number, canvas: HTMLCanvasElement) {
     super(stage, x, y, z, canvas);
     this.img = new Image();
@@ -18,16 +23,29 @@ export class Ground extends ObjDrone{
   }
 
   onDraw(): void {
+
+
+
     const x = this.canvas.width / 2;
     const y = this.canvas.height / 2;
     let context: CanvasRenderingContext2D = this.canvas.getContext("2d");
-    context.drawImage(this.img,x - this.img.width/2, this.canvas.height -  this.img.height);
+    if(this.beforeWind - this.wind>0){
+      this.currentX -= 0.5;
+    }else if(this.beforeWind - this.wind<0){
+      this.currentX += 0.5;
+    }
+    context.drawImage(this.img, (x - this.img.width/2) + this.currentX, this.canvas.height -  this.img.height);
   }
 
   clockSignal(value?: any) {
     this.onDraw();
   }
   intentSignal(intent: Intent<GameData>) {
+    if(this.beforeWind!=intent.data.wind.x){
+      this.beforeWind=intent.data.wind.x
+    }else{
+      this.wind=intent.data.wind.x
+    }
   }
 
 }
