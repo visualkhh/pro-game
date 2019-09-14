1c3102e0c23f2a4b9f733000a2f1ad97ca094a58
khh <visualkhh@gmail.com>
2019-09-14 16:07:27 +0900

    axis add

--- typescript/test-game/src/app/app.component.ts
+++ typescript/test-game/src/app/app.component.ts
@@ -19,7 +19,6 @@ export class AppComponent implements OnInit, AfterViewInit {
   onResize(event) {
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
-    //trigger
     this.canvas.dispatchEvent(new Event('resize'));
   }
 
@@ -38,11 +37,11 @@ export class AppComponent implements OnInit, AfterViewInit {
     // this.context.rect(0, 0, 300, 300);
     // this.context.stroke();
 
-    const start = new PointVector(0, 300);
-    const center = new PointVector(0, 400);
-    const end = new PointVector(300, 0);
+    const start = new PointVector(20, 300);
+    const center = new PointVector(100, 20);
+    const end = new PointVector(200, 200);
+    const lastEnd = new PointVector(300, 30);
 
-    const position = start.get();
 
     this.context.beginPath();
     this.context.arc(start.x, start.y, 10, 0, 2 * Math.PI);
@@ -53,36 +52,56 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.context.beginPath();
     this.context.arc(end.x, end.y, 10, 0, 2 * Math.PI);
     this.context.stroke();
+    this.context.beginPath();
+    this.context.arc(lastEnd.x, lastEnd.y, 10, 0, 2 * Math.PI);
+    this.context.stroke();
 
 
     const frame = 50;
     const sD = new PointVector((start.x - center.x) / frame, (start.y - center.y) / frame);
     const cD = new PointVector((center.x - end.x) / frame, (center.y - end.y) / frame);
-    const eD = new PointVector(end.x / frame, end.y / frame);
+    const eD = new PointVector((end.x - lastEnd.x) / frame, (end.y - lastEnd.y) / frame);
+    const leD = new PointVector(lastEnd.x / frame, lastEnd.y / frame);
 
 
     interval(100).take(frame).subscribe(it => {
       start.sub(sD);
       center.sub(cD);
+      end.sub(eD);
       // this.context.beginPath();
-      // this.context.arc(start.x, start.y, 10, 0, 2 * Math.PI);
+      // this.context.arc(start.x, start.y, 5, 0, 2 * Math.PI);
       // this.context.stroke();
-      //
+      // //
       // this.context.beginPath();
-      // this.context.arc(center.x, center.y, 10, 0, 2 * Math.PI);
+      // this.context.arc(center.x, center.y, 5, 0, 2 * Math.PI);
       // this.context.stroke();
-
+      // //
+      // this.context.beginPath();
+      // this.context.arc(end.x, end.y, 5, 0, 2 * Math.PI);
+      // this.context.stroke();
 
       const cnt = it + 1;
       // console.log(it)
 
       const dD = new PointVector(((start.x - center.x) / frame) * cnt, ((start.y - center.y) / frame) * cnt);
+      const ddd = new PointVector(start.x - dD.x, start.y - dD.y);
+
+
+      const ldD = new PointVector(((center.x - end.x) / frame) * cnt, ((center.y - end.y) / frame) * cnt);
+      const lddd = new PointVector(center.x - ldD.x, center.y - ldD.y);
+
+      const gdD = new PointVector(((ddd.x - lddd.x) / frame) * cnt, ((ddd.y - lddd.y) / frame) * cnt);
+      const gddd = new PointVector(ddd.x - gdD.x, ddd.y - gdD.y);
       // position.sub(dD)
       this.context.beginPath();
-      this.context.arc(start.x - dD.x, start.y - dD.y, 1, 0, 2 * Math.PI);
+      // this.context.strokeStyle = '#FF0000';
+      // this.context.arc(ddd.x, ddd.y, 1, 0, 2 * Math.PI);
+      this.context.arc(gddd.x, gddd.y, 1, 0, 2 * Math.PI);
+      // this.context.arc(lddd.x, lddd.y, 1, 0, 2 * Math.PI);
+      // this.context.arc(ddd.x - lddd.x, ddd.y - lddd.y, 1, 0, 2 * Math.PI);
       this.context.stroke();
 
-      console.log('-->', dD.x, dD.y);
+      // console.log('-->', ddd.x / 100, ddd.x, ddd.y);
       //
       // this.context.beginPath();
       // this.context.moveTo(start.x,  start.y);
