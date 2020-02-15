9f0a484f528c1eadad03dcf3e4b0dc53ade0796c
khh <visualkhh@gmail.com>
2020-02-15 12:41:24 +0900

    g

--- typescript/test-game/src/app/app.component.ts
+++ typescript/test-game/src/app/app.component.ts
@@ -67,7 +67,6 @@ export class AppComponent implements OnInit, AfterViewInit {
     const end = new PointVector(RandomUtil.random(10, window.innerWidth), RandomUtil.random(10, window.innerHeight));
     const lastEnd = new PointVector(RandomUtil.random(10, window.innerWidth), RandomUtil.random(10, window.innerHeight));
 
-
     this.context.beginPath();
     this.context.arc(start.x, start.y, 10, 0, 2 * Math.PI);
     this.context.stroke();
@@ -81,65 +80,35 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.context.arc(lastEnd.x, lastEnd.y, 10, 0, 2 * Math.PI);
     this.context.stroke();
 
-
-    const frame = 500;
+    const frame = 70;
     const sD = new PointVector((start.x - center.x) / frame, (start.y - center.y) / frame);
     const cD = new PointVector((center.x - end.x) / frame, (center.y - end.y) / frame);
     const eD = new PointVector((end.x - lastEnd.x) / frame, (end.y - lastEnd.y) / frame);
     const leD = new PointVector(lastEnd.x / frame, lastEnd.y / frame);
 
-
     interval(10).take(frame).subscribe(it => {
       start.sub(sD);
       center.sub(cD);
       end.sub(eD);
-      // this.context.beginPath();
-      // this.context.arc(start.x, start.y, 5, 0, 2 * Math.PI);
-      // this.context.stroke();
-      // //
-      // this.context.beginPath();
-      // this.context.arc(center.x, center.y, 5, 0, 2 * Math.PI);
-      // this.context.stroke();
-      // //
-      // this.context.beginPath();
-      // this.context.arc(end.x, end.y, 5, 0, 2 * Math.PI);
-      // this.context.stroke();
-
       const cnt = it + 1;
-      // console.log(it)
-
       const dD = new PointVector(((start.x - center.x) / frame) * cnt, ((start.y - center.y) / frame) * cnt);
       const ddd = new PointVector(start.x - dD.x, start.y - dD.y);
-
-
       const ldD = new PointVector(((center.x - end.x) / frame) * cnt, ((center.y - end.y) / frame) * cnt);
       const lddd = new PointVector(center.x - ldD.x, center.y - ldD.y);
 
       const gdD = new PointVector(((ddd.x - lddd.x) / frame) * cnt, ((ddd.y - lddd.y) / frame) * cnt);
       const gddd = new PointVector(ddd.x - gdD.x, ddd.y - gdD.y);
-      // position.sub(dD)
-      this.context.clearRect(0, 0, 9999, 9999);
       this.context.beginPath();
-      // this.context.strokeStyle = '#FF0000';
-      // this.context.arc(ddd.x, ddd.y, 1, 0, 2 * Math.PI);
       this.context.arc(gddd.x, gddd.y, 3, 0, 2 * Math.PI);
-      this.context.drawImage(moon, gddd.x - 100, gddd.y - 100)
-      this.context.drawImage(ducktaek, ddd.x - 300, ddd.y - 200)
-      this.context.drawImage(duckboon, lddd.x, lddd.y - 200)
-      // this.context.arc(lddd.x, lddd.y, 1, 0, 2 * Math.PI);
-      // this.context.arc(ddd.x - lddd.x, ddd.y - lddd.y, 1, 0, 2 * Math.PI);
       this.context.stroke();
-
-      // console.log('-->', ddd.x / 100, ddd.x, ddd.y);
-      //
-      // this.context.beginPath();
-      // this.context.moveTo(start.x,  start.y);
-      // this.context.lineTo(center.x,  center.y);
-      // this.context.stroke();
-      // this.context.beginPath();
-      // this.context.moveTo(center.x,  center.y);
-      // this.context.lineTo(end.x,  end.y);
-      // this.context.stroke();
+      this.context.beginPath();
+      this.context.moveTo(start.x,  start.y);
+      this.context.lineTo(center.x,  center.y);
+      this.context.stroke();
+      this.context.beginPath();
+      this.context.moveTo(center.x,  center.y);
+      this.context.lineTo(end.x,  end.y);
+      this.context.stroke();
     });
   }
 }
