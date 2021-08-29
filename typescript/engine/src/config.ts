d1791e1f2dadd9753ebdff511a7eaa2444a5120e
khh <visualkhh@gmail.com>
2021-08-29 12:22:53 +0900

    Move Probability Center

--- typescript/engine/src/config.ts
+++ typescript/engine/src/config.ts
@@ -1,10 +1,10 @@
 const config = {
     // G: 6.67384 * Math.pow(10,-11),
-    G: 6.67384,
+    G: 1.67384,
     // G: 1.67384,
     // G: 0.2,
     // G: 16.67384,
-    drawDelay: 100,
+    drawDelay: 10,
 };
 
 export {config}
